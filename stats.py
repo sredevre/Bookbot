0b8968ebf8e297be0ec8076e08726ebc984a9bea
sredevre <sreevedhalla@gmail.com>
2026-08-26 10:27:44 +1000

    completion of sort_on

--- stats.py
+++ stats.py
@@ -5,6 +5,7 @@ def get_book():
         string = f.read()
     return string
 
+
 def main():
     string  = get_book()
     count = string.split()
@@ -13,7 +14,7 @@ def main():
 
 print(main())
 
-# This is the letter counting thing part of the assessment
+# This is the letter counting part of the assessment
 
 
 def return_characters(string):
@@ -30,9 +31,16 @@ def return_characters(string):
 
 print(return_characters(get_book()))
 
+# This is the sorting part of the assessment
 
-def sortation(letters):
-    sorted_list = []
+def sort_on(letters: tuple[str, int]) -> int:
+    return letters[1]
+
+
+def chars_dict_to_sorted_list(letters):
+    
+    letters2 = letters:list[tuple[str, int]]
+    return letters2
 
     letters = return_characters()
 
